f445849a080d79d3f02e879a044ac4249859ca20
Guillermo Fuentes <guillermofuentesquijada@gmail.com>
2017-10-12 12:43:32 -0300

    commit 2

--- app/src/main/java/com/gps/mapa/MapsActivity.java
+++ app/src/main/java/com/gps/mapa/MapsActivity.java
@@ -9,17 +9,20 @@ import android.support.v4.content.ContextCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationSource {
 
     private static final int REQUEST_PERMISSION_FINE_GPS = 1;
     private static final int REQUEST_PERMISSION_COARSE_GPS = 2;
     private static final int REQUEST_PERMISSION_INTERNET = 3;
     private GoogleMap mMap;
+
+    private OnLocationChangedListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +38,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        mMap.setLocationSource(this);
+
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
@@ -92,5 +103,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 break;
             }
         }
+    }
+
+    @Override
+    public void activate(OnLocationChangedListener onLocationChangedListener) {
+        listener = onLocationChangedListener;
+    }
+
+    @Override
+    public void deactivate() {
+
     }
 }
